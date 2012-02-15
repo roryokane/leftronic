c060df97bac57dca7a12e1b8f184c817362611f1
Cesar Del Solar <delsolar@gmail.com>
2012-02-14 16:36:00 -0800

    new python api ready

--- leftronic.py
+++ leftronic.py
@@ -8,10 +8,13 @@ class Leftronic(object):
     def __init__(self, authKey):
         # Sets accessKey
         self.accessKey = authKey
-        self.apiUrl = 'https://beta.leftronic.com/customSend/'
+        self.apiUrl = 'http://dev.leftronic.com/customSend/'
 
     def pushNumber(self, streamName, point):
-        """ Pushes a number to a Number, Horizontal/Vertical Bar, or Dial widget. """
+        """ 
+        Pushes a number to a Number, Horizontal/Vertical Bar, Dial widget, Stoplight.
+        or sparkline/line graph
+        """
         if type(point) == float or type(point) == long or type(point) == int:
             point = {'number': point}
         elif type(point) == list:
@@ -27,7 +30,7 @@ class Leftronic(object):
         else:
             raise TypeError
         parameters = {'accessKey': self.accessKey, 'streamName': streamName, 'point': point}
-        self.postData(parameters)
+        return self.postData(parameters)
 
     def pushGeo(self, streamName, lati, longi, color=None):
         """
@@ -35,35 +38,114 @@ class Leftronic(object):
         Color can also be passed (red, green, blue, yellow, or purple).
         Default color is red.
         """
-        parameters = {'accessKey': self.accessKey, 'streamName': streamName, 'point': {
-            'latitude': lati, 'longitude': longi, 'color': color
-        }}
-        self.postData(parameters)
+        if type(lati) != list and type(longi) != list and type(color) != list:
+            
+            point = {'latitude': lati, 'longitude': longi}
+            if color:
+                point['color'] = color
+                         
+        elif type(lati) == list and type(longi) == list:
+            if len(lati) != len(longi):
+                raise ValueError
+            point = []
+            for i in range(len(lati)):
+                obj = {'latitude': lati[i], 'longitude': longi[i]}
+                if color and type(color) == list and color[i]:
+                    obj['color'] = color[i]
+                point.append(obj)
+        else:
+            raise TypeError
+        parameters = {'accessKey': self.accessKey, 'streamName': streamName, 
+                      'point': point}
+        return self.postData(parameters)
 
-    def pushText(self, streamName, myTitle, myMsg):
+    def pushText(self, streamName, myTitle, myMsg, imgUrl=None):
         """ Pushes a title and message to a Text Feed widget. """
-        parameters = {'accessKey': self.accessKey, 'streamName': streamName, 'point': {
-            'title': myTitle, 'msg': myMsg
-        }}
-        self.postData(parameters)
+        if type(myTitle) != list and type(myMsg) != list and type(imgUrl) != list:
+            point = {'title': myTitle, 'msg': myMsg}
+            if imgUrl:
+                point['imgUrl'] = imgUrl
+        elif type(myTitle) == list and type(myMsg) == list:
+            if len(myTitle) != len(myMsg):
+                raise ValueError
+            point = []
+            for i in range(len(myTitle)):
+                obj = {'title': myTitle[i], 'msg': myMsg[i]}
+                if imgUrl and type(imgUrl) == list and imgUrl[i]:
+                    obj['imgUrl'] = imgUrl[i]
+                point.append(obj)
+        else:
+            raise TypeError
+
+        parameters = {'accessKey': self.accessKey, 'streamName': streamName, 'point': point}
+        return self.postData(parameters)
 
     def pushLeaderboard(self, streamName, leaderArray):
         """ Pushes an array to a Leaderboard widget. """
         parameters = {'accessKey': self.accessKey, 'streamName': streamName, 'point': {
             'leaderboard': leaderArray
         }}
-        self.postData(parameters)
+        return self.postData(parameters)
 
     def pushList(self, streamName, listArray):
         """ Pushes an array to a List widget. """
+        if type(listArray) != list:
+            raise TypeError
+        for i in range(len(listArray)):
+            if type(listArray[i]) != dict:
+                listArray[i] = {'listItem': listArray[i]}
+                
         parameters = {'accessKey': self.accessKey, 'streamName': streamName, 'point': {
             'list': listArray
         }}
-        self.postData(parameters)
-        
+        return self.postData(parameters)
+    
+    def pushPair(self, streamName, x, y):
+        """ Pushes an x,y pair to a Pair widget"""
+        if type(x) == list and type(y) == list:
+            point = []
+            if len(x) != len(y): raise ValueError
+            for i in range(len(x)):
+                point.append({'x': x[i], 'y': y[i]})
+        else:
+            point = {'x': x, 'y': y}
+        parameters = {'accessKey': self.accessKey, 'streamName': streamName, 
+                      'point': point}
+        return self.postData(parameters)
+
+    def pushImage(self, streamName, imgUrl):
+        """ Pushes an image to an Image widget"""
+        point = {'imgUrl': imgUrl}
+        parameters = {'accessKey': self.accessKey, 'streamName': streamName, 
+                      'point': point}
+        return self.postData(parameters)
+
+    def pushLabel(self, streamName, label):
+        """ Pushes a label to a Label widget"""
+        point = {'label': label}
+        parameters = {'accessKey': self.accessKey, 'streamName': streamName, 
+                      'point': point}
+        return self.postData(parameters)
+
+    def pushTable(self, streamName, headerRow, dataRows):
+        """ Pushes a table to a Table widget """
+        dataRows.insert(0, headerRow)
+        point = {'table': dataRows}
+        parameters = {'accessKey': self.accessKey, 'streamName': streamName, 
+                      'point': point}
+        return self.postData(parameters)
+
+    def clear(self, streamName):
+        parameters = {'accessKey': self.accessKey, 'streamName': streamName, 
+                      'command': 'clear'}
+        return self.postData(parameters)
+
     def postData(self, parameters):
         """ Makes an HTTP POST to the API URL. """
         # Convert to JSON
         jsonData = json.dumps(parameters)
         # Make request
-        urllib2.urlopen(self.apiUrl, jsonData)
+        response = urllib2.urlopen(self.apiUrl, jsonData)
+        return response.read()
+
+    
